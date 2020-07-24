76e942c508efbca8abd6d03fb1bdd1e819a9c0c0
James Christian Wira <jamesch888@gmail.com>
2020-07-24 23:43:47 +0700

    Adding Firestore support and validation on SplashScreen

--- app/src/main/java/com/potatodev/chatto/activities/SplashScreenActivity.java
+++ app/src/main/java/com/potatodev/chatto/activities/SplashScreenActivity.java
@@ -1,12 +1,20 @@
 package com.potatodev.chatto.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.potatodev.chatto.R;
 import com.potatodev.chatto.preferences.SPreferences;
 
@@ -16,6 +24,7 @@ import java.util.TimerTask;
 public class SplashScreenActivity extends AppCompatActivity {
 
     private boolean isAuth;
+    private String password;
     public static SharedPreferences authentication;
 
     @Override
@@ -30,20 +39,49 @@ public class SplashScreenActivity extends AppCompatActivity {
         */
         authentication = getSharedPreferences(SPreferences.getPreferenceFilename(), SPreferences.getPreferenceMode());
         isAuth = authentication.getBoolean(SPreferences.getKeyIsAuth(), false);
+        password = authentication.getString(SPreferences.getKeyPass(), "");
 
-        // Delay on splash screen
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        DocumentReference docRef = firestore.collection("authPassword").document("7Y0Hc1V00Mwfp0Ygmzqe");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void run() {
-                // Page designation for each authentication result
-                if (!isAuth){
-                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-                } else {
-                    startActivity(new Intent(SplashScreenActivity.this, StartActivity.class));
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot snapshot = task.getResult();
+                if (snapshot.exists()) {
+                    final String currentPass = (String) snapshot.get("currentPassword");
+
+                    // Delay on splash screen
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            // Page designation for each authentication result
+                            if (isAuth || password.equals(currentPass)){
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        showToast("Welcome back!", Toast.LENGTH_LONG);
+                                    }
+                                });
+                                startActivity(new Intent(SplashScreenActivity.this, StartActivity.class));
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        showToast("Your current password doesn't match. Please the password to the developer", Toast.LENGTH_LONG);
+                                    }
+                                });
+                                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                            }
+                            finish();
+                        }
+                    }, 3000);
                 }
-                finish();
             }
-        }, 3000);
+        });
+    }
+
+    public void showToast(String message, int duration){
+        Toast.makeText(this, message, duration).show();
     }
 }
